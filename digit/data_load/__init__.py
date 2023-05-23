c69bc053e31c955e78e0e84d4fb38994c3f60c25
removedporn <86824510+removedporn@users.noreply.github.com>
2023-05-23 21:19:02 +0800

    Delete digit directory

--- digit/data_load/__init__.py
+++
@@ -1,3 +0,0 @@
-from .svhn import *
-from .mnist import *
-from .usps import *
--- digit/digit.sh
+++
@@ -1,3 +0,0 @@
-~/anaconda3/envs/pytorch/bin/python uda_digit.py --dset m2u --gpu_id 0 --cls_par 0.1 --output ckps_digits
-~/anaconda3/envs/pytorch/bin/python uda_digit.py --dset u2m --gpu_id 0 --cls_par 0.1 --output ckps_digits
-~/anaconda3/envs/pytorch/bin/python uda_digit.py --dset s2m --gpu_id 0 --cls_par 0.1 --output ckps_digits
